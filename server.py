b19e7bc7d8be325438312f68bdafc0b3253233e7
Curtis Mason <cumason@bu.edu>
2018-11-11 13:30:57 -0500

    Percentages

--- server.py
+++ server.py
@@ -3,10 +3,17 @@ from flask import request
 from libsafewhey import SafeWhey
 import distance_calc
 import googlemaps
+import numpy as np
 app = Flask(__name__)
 
 gmaps_key = "AIzaSyA-L1-CDamtVsUcngy65o54omBY_wn9TT0"
 gmaps = googlemaps.Client(key = gmaps_key)
+
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
 
 @app.route('/')
 def hello_world():
@@ -26,11 +33,13 @@ def map():
 			return render_template('index.html', err='enter valid address')
 	else:
 		directions_result = distance_calc.create_directions_result()
-
+	distr = softmax(vals['safety_vals'])
+	vals['safety_vals'] = [round(val) for val in vals['safety_vals']]
+	distr = [round(val*100)/100 for val in distr]
 	return render_template('goog_map_example.html', results=vals['safety_vals'], 
 		start=request.form['start'], end=request.form['dest'], mode="WALKING", 
 		origin=start_coord, num_routes=vals['num_routes'], holder=0, 
-		routeBoxes = vals['routeBoxes'])
+		routeBoxes = vals['routeBoxes'], distr = distr)
 
 if __name__ == '__main__':
 	app.run(debug=True)
