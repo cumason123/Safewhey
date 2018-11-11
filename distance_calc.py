774f4b1d811fb668ed390a682525117f17c9232d
Damani Philip <38143975+d-philip@users.noreply.github.com>
2018-11-11 00:03:27 -0500

    Implemented multiple route choice.

--- distance_calc.py
+++ distance_calc.py
@@ -10,14 +10,16 @@ origin_long = -71.097074
 dest_lat = 42.373486
 dest_long = -71.120346
 
-origin = str(origin_lat) + "," + str(origin_long)
-destination = str(dest_lat) + "," + str(dest_long)
+origin = str(origin_lat) + ", " + str(origin_long)
+destination = str(dest_lat) + ", " + str(dest_long)
 
-directions_result = gmaps.directions(origin, destination, mode="transit", departure_time=now)
+def create_directions_result():
+    directions_result = gmaps.directions(origin, destination, mode="driving", alternatives=True)
+    return directions_result
 
-print(directions_result)
-
-
+def create_directions_url(): # use to embed in webpage
+    directions_url = "https://maps.google.com/maps/api/js?origin=" + origin + "&destination=" + destination + "&key=" + gmaps_key + "&callback=initMap"
+    return directions_url
 
 # KAPPA SHIT
 # origin_req = "origin="+ str(origin_lat) + "," + str(origin_long)
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+import distance_calc
 app = Flask(__name__)
+
+gmaps_key = "AIzaSyA-L1-CDamtVsUcngy65o54omBY_wn9TT0"
 
 @app.route('/')
 def hello_world():
@@ -7,7 +10,9 @@ def hello_world():
 
 @app.route('/map')
 def map():
-
+    directions_result = distance_calc.create_directions_result()
+    #result = distance_calc.create_directions()
+    return render_template('goog_map_example.html', result=directions_result)
 
 if __name__ == '__main__':
     app.run(debug=True)
